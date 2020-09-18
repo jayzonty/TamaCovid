00aabda356dc8505e9d5538d6cb0f89c6bd112c9
Jayzon Ty <jayzon.ty@gmail.com>
2020-09-18 16:49:47 +0800

    Added a class to contain data related to the game state.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,34 @@
+﻿
+namespace TamaCovid
+{
+    /// <summary>
+    /// Class containing data related
+    /// to the current state of the game.
+    /// </summary>
+    public class GameState
+    {
+        #region Daily Stats
+
+        /// <summary>
+        /// Player's money.
+        /// </summary>
+        public int money = 0;
+
+        /// <summary>
+        /// Player's hunger value.
+        /// </summary>
+        public int hunger = 0;
+
+        /// <summary>
+        /// Player's happiness value.
+        /// </summary>
+        public int happiness = 0;
+        
+        /// <summary>
+        /// Player's anxiety level.
+        /// </summary>
+        public int anxiety = 0;
+
+        #endregion
+    }
+}
