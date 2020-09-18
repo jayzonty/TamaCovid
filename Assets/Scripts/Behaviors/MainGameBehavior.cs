5243a1592d45caba17b77ac6d91288052f72a5de
Jayzon Ty <jayzon.ty@gmail.com>
2020-09-19 00:02:12 +0800

    Added a MonoBehavior and UI objects for the textbox
    - Also added dummy text for different states of the game

--- Assets/Scripts/Behaviors/MainGameBehavior.cs
+++ Assets/Scripts/Behaviors/MainGameBehavior.cs
@@ -11,6 +11,8 @@ namespace TamaCovid
         // TODO: Separate functionality of each state into separate classes,
         // something like a "Sequence" class.
 
+        public TextBoxBehavior textBox;
+
         /// <summary>
         /// States within the gameplay loop
         /// </summary>
@@ -44,21 +46,76 @@ namespace TamaCovid
             switch (CurrentState)
             {
                 case State.Intro:
+
+                    // For now, just jump to wake up sequence, and set the text about waking up
+                    textBox.SetText("You woke up at 8AM.");
+                    CurrentState = State.WakeUpSequence;
+
                     break;
 
                 case State.WakeUpSequence:
+
+                    // Temporary.
+                    // If player presses the next dialogue button,
+                    // [roceed to activities selection.
+                    if (IsProceedDialogButtonPressed())
+                    {
+                        textBox.SetText("Choose activities for the day.");
+                        CurrentState = State.ActivitiesSelection;
+                    }
+
                     break;
 
                 case State.ActivitiesSelection:
+
+                    // Temporary
+                    // If player presses the next dialogue button,
+                    // proceed to perform activities.
+                    if (IsProceedDialogButtonPressed())
+                    {
+                        textBox.SetText("Perform activities.");
+                        CurrentState = State.PerformActivities;
+                    }
+
                     break;
 
                 case State.PerformActivities:
+
+                    // Temporary
+                    // If player presses the next dialogue button,
+                    // proceed to sleep preparation.
+                    if (IsProceedDialogButtonPressed())
+                    {
+                        textBox.SetText("Prepare to sleep.");
+                        CurrentState = State.PrepareToSleep;
+                    }
+
                     break;
 
                 case State.PrepareToSleep:
+
+                    // Temporary
+                    // If player presses the next dialogue button,
+                    // sleep.
+                    if (IsProceedDialogButtonPressed())
+                    {
+                        textBox.SetText("Sleeping... zzzzz");
+                        CurrentState = State.Sleep;
+                    }
+
                     break;
 
                 case State.Sleep:
+
+                    // Temporary
+                    // If player presses the next dialogue button,
+                    // wake up
+                    if (IsProceedDialogButtonPressed())
+                    {
+                        textBox.SetText("You woke up at 8AM.");
+                        CurrentState = State.WakeUpSequence;
+                    }
+
                     break;
 
                 case State.GameEnd:
@@ -68,6 +125,15 @@ namespace TamaCovid
                     Debug.LogError("Reach an invalid state!");
                     break;
             }
+        }
+
+        /// <summary>
+        /// Did the player press the next dialogue button during the current frame?
+        /// </summary>
+        /// <returns>True if the player pressed the next dialogue button during the current frame.</returns>
+        private bool IsProceedDialogButtonPressed()
+        {
+            return Input.GetKeyDown(KeyCode.Space);
         }
     }
 }
---
+++ Assets/Scripts/UI/TextBoxBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TamaCovid
+{
+    /// <summary>
+    /// Class describing the behavior of
+    /// the generic text box.
+    /// </summary>
+    public class TextBoxBehavior : MonoBehaviour
+    {
+        /// <summary>
+        /// Text component that displays the text string.
+        /// </summary>
+        public Text textComponent;
+
+        /// <summary>
+        /// Sets the text being displayed by the text box.
+        /// </summary>
+        /// <param name="text"></param>
+        public void SetText(string text)
+        {
+            if (text != null)
+            {
+                textComponent.text = text;
+            }
+        }
+
+    }
+}
