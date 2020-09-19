d269cf9d04fa1dc66a47ec2ec53036ac3c44c415
Jayzon Ty <jayzon.ty@gmail.com>
2020-09-19 15:27:48 +0800

    Added StatType enum, StateEffect class, and a template scriptable object for in-game actions

---
+++ Assets/Scripts/Enums/StatType.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TamaCovid
+{
+    /// <summary>
+    /// Enum for types of stats
+    /// </summary>
+    public enum StatType
+    {
+        Money,
+
+        Hunger,
+        Happiness,
+        Anxiety,
+        Energy,
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/SO_Action.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace TamaCovid
+{
+    /// <summary>
+    /// Scriptable object that contains data about
+    /// in-game actions.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Action", menuName = "TamaCovid/Action")]
+    public class SO_Action : ScriptableObject
+    {
+        /// <summary>
+        /// Category of the action
+        /// </summary>
+        public string category;
+
+        /// <summary>
+        /// Description of the action
+        /// </summary>
+        public string description;
+
+        // TODO: Add data regarding conditions for
+        // allowing the action to be performed.
+
+        // TODO: Add data for dialogue lines and triggers
+        // to change the game state, or to set/unset flags, etc.
+    }
+}
