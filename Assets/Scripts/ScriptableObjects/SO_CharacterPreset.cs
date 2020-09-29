c9f76b22b048dc7c5297d1419fca17d97c0e7537
Jayzon Ty <jayzon.ty@gmail.com>
2020-09-29 23:23:40 +0800

    Added a scriptable object for character presets

---
+++ Assets/Scripts/ScriptableObjects/SO_CharacterPreset.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace TamaCovid
+{
+    /// <summary>
+    /// Scriptable object containing data about a
+    /// character preset.
+    /// </summary>
+    public class SO_CharacterPreset : ScriptableObject
+    {
+        /// <summary>
+        /// Name of the preset.
+        /// </summary>
+        public string presetName = "Character Preset";
+
+        /// <summary>
+        /// Description for the preset.
+        /// </summary>
+        public string description = "Description";
+
+        /// <summary>
+        /// Starting money
+        /// </summary>
+        public int startingMoney = 0;
+
+        /// <summary>
+        /// Starting job
+        /// </summary>
+        public int startingJob = 0;
+
+        /// <summary>
+        /// Does the character possess a car?
+        /// </summary>
+        public bool hasCar = false;
+
+        /// <summary>
+        /// Is the character immunocompromised?
+        /// </summary>
+        public bool isImmunocompromised = false;
+
+        /// <summary>
+        /// Does the character have depression?
+        /// </summary>
+        public bool hasDepression = false;
+    }
+}
